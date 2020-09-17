ab607e5bd738292b77a75f8d802e882ccbe2e62d
Vinicius Alves <vinicius_o.a@live.com>
2020-09-17 14:55:23 -0300

    rebuilding site qui set 17 14:55:23 -03 2020

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+rm -rf public
+hugo -t cactus
+git add -A
+
+# Commit message.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+git commit -m "$msg"
+git push origin master
+git subtree push --prefix=public git@github.com:ovalves/ovalves.github.io.git gh-pages
